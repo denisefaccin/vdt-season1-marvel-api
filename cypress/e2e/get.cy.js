b0f35035a08a75e760695579e171ddbb555cde95
Denise Faccin <denisefaccin@gmail.com>
2022-07-13 12:05:49 -0300

    format test files

--- cypress/e2e/get.cy.js
+++ cypress/e2e/get.cy.js
@@ -1,23 +1,23 @@
-describe("GET/characters", function () {
+describe('GET/characters', function () {
   const characters = [
     {
-      name: "Charles Xavier",
-      alias: "Professor X",
-      team: ["x-men"],
-      active: true,
+      name: 'Charles Xavier',
+      alias: 'Professor X',
+      team: ['x-men'],
+      active: true
     },
     {
-      name: "Logan",
-      alias: "Wolverine",
-      team: ["vingadores"],
-      active: true,
+      name: 'Logan',
+      alias: 'Wolverine',
+      team: ['vingadores'],
+      active: true
     },
     {
-      name: "Peter Parker",
-      alias: "Homem Aranha",
-      team: ["novos vingadores"],
-      active: true,
-    },
+      name: 'Peter Parker',
+      alias: 'Homem Aranha',
+      team: ['novos vingadores'],
+      active: true
+    }
   ];
 
   before(function () {
@@ -25,57 +25,57 @@ describe("GET/characters", function () {
     cy.setToken();
     cy.populateCharacters(characters);
   });
-  it("Deve retornar uma lista de personagens", function () {
+  it('Deve retornar uma lista de personagens', function () {
     cy.getCharacters().then(function (response) {
       expect(response.status).to.eql(200);
-      expect(response.body).to.be.a("array");
+      expect(response.body).to.be.a('array');
       expect(response.body.length).greaterThan(0);
     });
   });
 
-  it("Deve buscar personagem por nome", function () {
-    cy.getCharacters("Logan").then(function (response) {
+  it('Deve buscar personagem por nome', function () {
+    cy.getCharacters('Logan').then(function (response) {
       expect(response.status).to.eql(200);
       expect(response.body.length).to.eql(1);
-      expect(response.body[0].alias).to.eql("Wolverine");
+      expect(response.body[0].alias).to.eql('Wolverine');
       expect(response.body[0].active).to.eql(true);
-      expect(response.body[0].time).to.eql("[x-men]");
+      expect(response.body[0].time).to.eql('[x-men]');
     });
   });
 });
 
-describe("GET/characters/id", function () {
+describe('GET/characters/id', function () {
   before(function () {
     cy.bac2ThePast();
     cy.setToken();
   });
 
   const tonyStark = {
-    name: "Tony Stark",
-    alias: "Homem de Ferro",
-    team: ["Vingadores"],
-    active: true,
+    name: 'Tony Stark',
+    alias: 'Homem de Ferro',
+    team: ['Vingadores'],
+    active: true
   };
 
-  context("Quando tenho um personagem cadastrado", function () {
+  context('Quando tenho um personagem cadastrado', function () {
     before(function () {
-      //TODO
+      // TODO
       cy.postCharacter(tonyStark).then(function (response) {
-        Cypress.env("characterId", response.body.character_id);
+        Cypress.env('characterId', response.body.character_id);
       });
     });
-    it("Deve buscar o personagem pelo ID", function () {
+    it('Deve buscar o personagem pelo ID', function () {
       cy.getCharacterById(id).then(function (response) {
         expect(response.status).to.eql(200);
-        expect(response.body.alias).to.eql("Homem de Ferro");
+        expect(response.body.alias).to.eql('Homem de Ferro');
         expect(response.body.active).to.eql(true);
-        expect(response.body.time).to.eql("Vingadores");
+        expect(response.body.time).to.eql('Vingadores');
       });
     });
   });
 
-  it("Deve retornar 404 ao buscar por ID não cadastrado", function () {
-    const id = "8r8t8686a68s868f868f";
+  it('Deve retornar 404 ao buscar por ID não cadastrado', function () {
+    const id = '8r8t8686a68s868f868f';
     cy.getCharacterById(id).then(function (response) {
       expect(response.status).to.eql(404);
     });
--- cypress/e2e/post.cy.js
+++ cypress/e2e/post.cy.js
@@ -1,21 +1,21 @@
-const cypress = require("cypress");
+const cypress = require('cypress');
 
-describe("POST /characters", function () {
+describe('POST /characters', function () {
   before(function () {
     cy.bac2ThePast();
     cy.setToken();
   });
 
-  it("Deve cadastrar um personagem", function () {
+  it('Deve cadastrar um personagem', function () {
     const character = {
-      name: "Wanda Maximoff",
-      alias: "Feiticeira Escarlate",
-      team: ["vingadores"],
-      active: true,
+      name: 'Wanda Maximoff',
+      alias: 'Feiticeira Escarlate',
+      team: ['vingadores'],
+      active: true
     };
     cy.postCharacter(character).then(function (response) {
       expect(response.status).to.equal(201);
-      //para aparecer no cypress run a resposta do body - id do character criado - requisito
+      // para aparecer no cypress run a resposta do body - id do character criado - requisito
       cy.log(response.body.character_id);
       expect(response.body.character_id.lenght).to.equal(24);
     });
@@ -23,56 +23,56 @@ describe("POST /characters", function () {
 
   before(function () {
     cy.api({
-      method: "POST",
-      url: "/characters",
+      method: 'POST',
+      url: '/characters',
       body: character,
       headers: {
-        Authorization: cypress.env("token"),
+        Authorization: cypress.env('token')
       },
-      failOnStatusCode: false,
+      failOnStatusCode: false
     }).then(function (response) {
       expect(response.status).to.equal(201);
-      //para aparecer no cypress run a resposta do body - id do character criado - requisito
+      // para aparecer no cypress run a resposta do body - id do character criado - requisito
       cy.log(response.body.character_id);
       expect(response.body.character_id.lenght).to.equal(24);
     });
   });
 
-  context("quando o personagem já existe", function () {
+  context('quando o personagem já existe', function () {
     const character = {
-      name: "Pietro Maximoff",
-      alias: "Mercurio",
-      team: ["vingadores da costa oeste", "irmandade de mutantes"],
-      active: true,
+      name: 'Pietro Maximoff',
+      alias: 'Mercurio',
+      team: ['vingadores da costa oeste', 'irmandade de mutantes'],
+      active: true
     };
 
     before(function () {
       cy.api({
-        method: "POST",
-        url: "/characters",
+        method: 'POST',
+        url: '/characters',
         body: character,
         headers: {
-          Authorization: Cypress.env("token"),
+          Authorization: Cypress.env('token')
         },
-        failOnStatusCode: false,
+        failOnStatusCode: false
       }).then(function (response) {
         expect(response.status).to.equal(201);
       });
     });
 
-    it("não deve cadastrar duplicado", function () {
+    it('não deve cadastrar duplicado', function () {
       cy.api({
-        method: "POST",
-        url: "/characters",
+        method: 'POST',
+        url: '/characters',
         body: character,
         headers: {
-          Authorization: Cypress.env("token"),
+          Authorization: Cypress.env('token')
         },
-        failOnStatusCode: false,
+        failOnStatusCode: false
       }).then(function (response) {
         expect(response.status).to.equal(400);
       });
-      expect(response.body.error).to.equal("Duplicate character");
+      expect(response.body.error).to.equal('Duplicate character');
     });
   });
 });
